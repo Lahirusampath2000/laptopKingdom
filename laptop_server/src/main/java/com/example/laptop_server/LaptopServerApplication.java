56804a7ec17d7d0cffe5388f80ac1e6c23134646
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-09-24 08:58:07 +0530

    initial commit

---
+++ laptop_server/src/main/java/com/example/laptop_server/LaptopServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptop_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopServerApplication.class, args);
+	}
+
+}
---
+++ laptop_server/src/test/java/com/example/laptop_server/LaptopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laptop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
