20d3f53bb93d64ac86559b0891a62897e7a19443
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-09-25 11:29:00 +0530

    config ui

--- laptop_ui/my-app/eslint.config.js
+++ laptop_ui/my-app/eslint.config.js
@@ -1,29 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx,mjs,cjs}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
+      ecmaFeatures: { jsx: true },
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: { react: pluginReact },
+    extends: ["plugin:react/recommended"],
+    rules: {
+      // optional: turn off the "React must be in scope" rule for React 17+
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "warn", // or "error" if you want stricter linting
+    },
+    settings: {
+      react: {
+        version: "detect", // auto-detect React version
       },
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
   },
-])
+]);
